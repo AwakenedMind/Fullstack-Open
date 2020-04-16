7e77e5b95299638bd47cad768263a16b5d4952aa
Robert Arteaga <robertarteaga@roberts-imac.attlocal.net>
2020-04-16 05:55:59 -0700

    added

--- section_2/phonebook/src/index.js
+++ section_2/phonebook/src/index.js
@@ -2,40 +2,33 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 
-const Filter = ({persons}) => {
-  const [ searchTerm, setSearchTerm ] = useState('');
-
-  const handleNameChange = (event) => {
-    console.log(event.target.value);
-    setSearchTerm(event.target.value);
-  };
-
-  React.useEffect(() => {
-    const results = persons.filter(person => {
-      person.name.toLowerCase().includes(searchTerm)
-    })
-  })
-
+const Filter = ({filter, onChange}) => {
   return (
     <>
     <h2>Find</h2>
     <div>
-      filter shown with <input id="name" onChange={handleNameChange} />
+      filter shown with <input value={filter} onChange={onChange} />
     </div> 
-    {results}
     </>
+  )
+}
+
+const Persons = ({persons}) => {
+  return(
+    <div>{persons}</div>
   )
 }
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '1' },
-    {name : 'Rob Sucks',  phone: '2'}
+    { name: 'Rob', number: '123' },
+    { name: 'Sky', number: '456' }
   ]) 
 
   // controlling the form input element.
-  const [ newName, setNewName ] = useState('')
-  const [newPhoneNum, setPhoneNum] = useState('')
+  const [ newName, setNewName ] = useState('');
+  const [newPhoneNum, setPhoneNum] = useState('');
+  const [filter, setFilter] = useState("")
 
   const doesUserExist = (event) => {
     event.preventDefault();
@@ -46,19 +39,30 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value);
-    setNewName(event.target.value);
-  };
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value);
-    setPhoneNum(event.target.value);
-  };
+    setPhoneNum(event.target.value)
+  }
+  
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase()))
+
+
+  const result = () => personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p> )
   
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter persons={persons} />
+      <Filter value={filter} onChange={handleFilterChange} />
       <form>
         <div>
           name: <input id="name" value={newName} onChange={handleNameChange} />
@@ -73,6 +77,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        <Persons persons={result()} />
+      </ul>
     </div>
   )
 }
